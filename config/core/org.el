e0a9f698bacf7cf737492a29a195bc6692e6aa11
Azhar Ibn Mostafiz <theazharul@gmail.com>
2024-12-12 10:26:07 +0600

    org updated

--- config/core/org.el
+++ config/core/org.el
@@ -1,36 +1,56 @@
 ;;; Org config
 (require 'org)
 
-;; Disable Subscripts Globally
-(setq org-use-sub-superscripts '{})
+;; Basic Org Settings
+(setq org-use-sub-superscripts '{}                  ;; Disable subscripts globally
+      org-log-done t                               ;; Log completion of tasks
+      org-startup-indented t                       ;; Start Org with indented content (better readability)
+      org-hide-leading-stars t                     ;; Hide leading stars in headings
+      org-pretty-entities t                        ;; Display pretty entities (e.g., Greek letters)
+      org-directory "~/Dropbox/aimacs/aimorg"      ;; Org directory
+      org-mobile-directory "~/Dropbox/aimacs/aimorg"
+      org-src-fontify-natively t                   ;; Syntax highlighting in source blocks
+      org-src-tab-acts-natively t                  ;; TAB acts natively in source blocks
+      org-src-window-setup 'current-window         ;; Use current window for editing source blocks
+      org-agenda-start-on-weekday 5                ;; Start agenda on Friday
+      org-default-notes-file (concat org-directory "/0. Inbox.org")) ;; Default notes file
 
-;; Enable logging when tasks are marked as done
-(setq org-log-done t)
+(with-eval-after-load 'org
+  ;; Org-specific configurations after org is loaded
+  (setq org-startup-indented nil                    ;; Disable startup indentation (override above)
+        org-special-ctrl-a/e t                      ;; Enable special C-a and C-e behavior
+        org-agenda-files (append
+                          (directory-files-recursively "~/Dropbox/aimacs/aimorg/" "\\.org$")
+                          (directory-files-recursively "~/Workspace/" "\\.org$"))
+        org-agenda-files (remove "~/Dropbox/aimacs/aimorg/4. Archives.org" org-agenda-files)
+        org-todo-keywords '((sequence "TODO(t)" "IN_PROGRESS(i)" "IN_REVIEW(r)" "|" "DONE(d)")
+                           (sequence "NEXT(n)" "WAITING(w@/)" "DELEGATED(D)" "HOLD(h@/)" "|" "CANCELLED(c@/)"))
+        org-global-properties '(("Effort_ALL" . "0:10 0:15 0:20 0:30 1:00 2:00 3:00 4:00 6:00 8:00"))
+        org-columns-default-format "%50ITEM(Task) %TODO %TAGS %SCHEDULED %DEADLINE %Effort(Estimated Effort){:} %CLOCKSUM"
+        create-lockfiles nil
+        org-archive-location "~/Dropbox/aimacs/aimorg/4. Archives.org::* From %s"
+        org-refile-targets '((org-agenda-files :maxlevel . 3))
+        org-capture-templates
+        '(("i" "Inbox" entry (file+headline "~/Dropbox/aimacs/aimorg/0. Inbox.org" "Inbox")
+           "* %? \n"))
+        ;; Keybindings
+        (global-set-key (kbd "C-c c") 'org-capture)
+        (global-set-key (kbd "C-c a") 'org-agenda)))
 
-;; Start Org with indented content (better readability)
-(setq org-startup-indented t)
-
-;; Hide leading stars in headings for a cleaner view
-(setq org-hide-leading-stars t)
-
-;; Display pretty entities, e.g., Greek letters
-(setq org-pretty-entities t)
-
-;; Optional: Enable org-appear for better visibility handling
+;; Org-appear for better visibility handling
 (use-package org-appear
-  :straight t
-  :config
-  (add-hook 'org-mode-hook #'org-appear-mode))
+  :ensure t
+  :hook (org-mode . org-appear-mode))
 
-;; Org-Superstar for better headline and item bullet aesthetics
+;; Org-superstar for better aesthetics
 (use-package org-superstar
-  :straight t
+  :ensure t
   :hook (org-mode . org-superstar-mode)
   :custom
-  (org-superstar-headline-bullets-list '("◉" "○" "✸" "✿")) ;; Customize the bullets
-  (org-superstar-item-bullet-alist '((?- . "•") (?- . "➤") (?- . "‣"))) ;; Customize item bullets
-  (org-hide-leading-stars t)        ;; Hide leading stars
-  (org-superstar-leading-bullet ?\s)) ;; Use a space for leading stars
+  (org-superstar-headline-bullets-list '("◉" "○" "✸" "✿"))
+  (org-superstar-item-bullet-alist '((?- . "•") (?- . "➤") (?- . "‣")))
+  (org-hide-leading-stars t)
+  (org-superstar-leading-bullet ?\s))
 
 ;; Define leader keybindings for Org mode
 (my-leader-def
@@ -43,4 +63,4 @@
   "o d" 'org-deadline              ;; Set a deadline for a task
 )
 
-(provide 'org)
+(provide 'org-config)
