5417a7927f4d9c9e6fe61a680546960ed13916df
Azhar Ibn Mostafiz <theazharul@gmail.com>
2025-01-16 15:52:56 +0600

    comment in init

--- init.el
+++ init.el
@@ -1,2 +1,8 @@
+;;; init.el --- This is the init file of my emacs configuration
+;;; Commentary:
+;;; Code:
 (org-babel-load-file (expand-file-name "./config.org" user-emacs-directory))
+
+(provide 'init)
+;;; init.el ends here
 
