7baa374060cd3aaaeb5d334b64f02c64ec28cc07
Azhar Ibn Mostafiz <azhar@silencer.theazharul.com>
2025-05-08 08:59:41 +0600

    updated

--- init.el
+++ init.el
@@ -1,8 +1,10 @@
-;;; init.el --- This is the init file of my emacs configuration
+;;; init.el --- Emacs configuration -*- lexical-binding: t; -*-
 ;;; Commentary:
 ;;; Code:
-(org-babel-load-file (expand-file-name "./config.org" user-emacs-directory))
+
+;; Ensure lexical binding for Org Babel loading
+(let ((lexical-binding t))
+  (org-babel-load-file (expand-file-name "./config.org" user-emacs-directory)))
 
 (provide 'init)
 ;;; init.el ends here
-
